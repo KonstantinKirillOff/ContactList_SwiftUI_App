da00813dcb27674d6cb8346ae37988e9b7a87a63
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-11-14 12:42:46 +0300

    Initial Commit

---
+++ ContactList_SwiftUI_App/ContactList_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList_SwiftUI_AppApp.swift
+//  ContactList_SwiftUI_App
+//
+//  Created by Константин Кириллов on 14.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactList_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
